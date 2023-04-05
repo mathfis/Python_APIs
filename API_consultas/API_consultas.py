14e1fd5d5f658af43c630083bdcbfd7667c6feb4
Matheus Lara <109297697+mathfis@users.noreply.github.com>
2023-04-05 19:31:35 -0300

    Add files via upload

---
+++ API_consultas/API_consultas.py
@@ -0,0 +1,147 @@
+from flask import Flask, jsonify, request
+import json
+
+# 1. Objetivo -  Desenvolver uma API que gerencia consultas médicas.
+
+# 2. URL base - localhost
+
+# 3. Endpoints - funcionalidades:
+
+# ** Pessoas Profissionais **
+# cadastrar profissional                     - localhost/pprofis (POST)
+# editar cadastro de pessoa profissional     - localhost/pprofis/id_pprofis (PUT)
+# deletar cadastro de pessoa profissional    - localhost/pprofis/id_pprofis (DELETE)
+
+# ** Consultas **
+# pesquisar consultas                        - localhost/consultas (GET)
+# pesquisar consultas  por id_profis         - localhost/consultas/id_pprofis (GET)
+# cadastrar profissional                     - localhost/consultas (POST)
+# editar cadastro de profissional            - localhost/consultas/id_pprofis (PUT)
+# deletar cadastro de profissional           - localhost/consultas/id_pprofis (DELETE)
+
+
+# 4. Quais recursos - livros
+# Abertura dos arquivos json
+
+#OBS:
+#    - Implementar mensagem de erro para o caso de criação de nova consulta
+#       com id_consulta existente
+with open("pessoas_profissionais.json","r", -1, "utf-8") as jsonFile:
+    pprofis = json.load(jsonFile)
+    jsonFile.close()
+
+with open("consultas.json","r", -1, "utf-8") as jsonFile:
+    consultas = json.load(jsonFile)
+    jsonFile.close()
+
+
+app = Flask(__name__)
+
+
+# Pesquisar todas as Pessoas Profissionais
+@app.route('/pprofis', methods=['GET'])
+def obter_pprofis():
+    return jsonify(pprofis)
+
+
+# Pesquisar todas as Consultas
+@app.route('/consultas', methods=['GET'])
+def obter_consultas():
+    return jsonify(consultas)
+
+
+# Pesquisar Consulta por id_pprofis
+@app.route('/consultas/<int:id_pprofis>', methods=['GET'])
+def pesquisar_consulta_id_pprofis(id_pprofis):
+    c_pprofis = [] # necessário caso a pessoa profissional
+                   # tenha mais de uma consulta registrada
+    for c in consultas:
+        if c.get("id_profissional") == id_pprofis:
+            c_pprofis.append(c)
+
+    return jsonify(c_pprofis)
+
+# # Cadastrar Pessoa Profissional
+@app.route('/pprofis', methods=['POST'])
+def cadastrar_pprofissional():
+    nova_pprofis = request.get_json()
+    pprofis.append(nova_pprofis)
+    # Escrevendo o arquivo json
+    with open("pessoas_profissionais.json", "w", -1, "utf-8") as jsonFile:
+        json.dump(pprofis,jsonFile)
+        jsonFile.close()
+
+    return jsonify(pprofis)
+
+
+# Cadastrar Consulta
+@app.route('/consultas', methods=['POST'])
+def cadastrar_consulta():
+    nova_consulta = request.get_json()
+    consultas.append(nova_consulta)
+    # Escrevendo o arquivo json
+    with open("consultas.json", "w", -1, "utf-8") as jsonFile:
+        json.dump(consultas, jsonFile)
+        jsonFile.close()
+
+    return jsonify(consultas)
+
+
+# Editar Pessoa Profissional
+@app.route('/pprofis/<int:id_pprofis>', methods=['PUT'])
+def editar_pprofis(id_pprofis):
+    pprofis_alterado = request.get_json()
+    for indice, p in enumerate(pprofis):
+        if p.get("id_profssional") == id_pprofis:
+            pprofis[indice].update(pprofis_alterado)
+            #Escrevendo o arquivo json
+            with open("pessoas_profissionais.json", "w", -1, "utf-8") as jsonFile:
+                json.dump(pprofis, jsonFile)
+                jsonFile.close()
+
+            return jsonify(pprofis[indice])
+
+
+# Editar Consulta
+@app.route('/consultas/<int:id_consulta>', methods=['PUT'])
+def editar_consulta_por_id(id_consulta):
+    consulta_alterada = request.get_json()
+    for indice, c in enumerate(consultas):
+        if c.get("id_consulta") == id_consulta:
+            consultas[indice].update(consulta_alterada)
+            #Escreve arquivo json
+            with open("consultas.json", "w", -1, "utf-8") as jsonFile:
+                json.dump(consultas, jsonFile)
+                jsonFile.close()
+
+            return jsonify(consultas[indice])
+
+
+# Deletar Pessoa Profissional
+@app.route('/pprofis/<int:id_pprofis>', methods=['DELETE'])
+def delete_pprofis(id_pprofis):
+    for indice, p in enumerate(pprofis):
+        if p.get("id_profssional") == id_pprofis:
+            del pprofis[indice]
+            # Escrevendo o arquivo json
+            with open("pessoas_profissionais.json", "w", -1, "utf-8") as jsonFile:
+                json.dump(pprofis, jsonFile)
+                jsonFile.close()
+            return jsonify(pprofis)
+
+
+# Deletar Consulta
+@app.route('/consultas/<int:id_consulta>', methods=['DELETE'])
+def delete_consulta(id_consulta):
+    for indice, c in enumerate(consultas):
+        if c.get("id_consulta") == id_consulta:
+            del consultas[indice]
+            # Escrevendo o arquivo json
+            with open("pessoas_profissionais.json", "w", -1, "utf-8") as jsonFile:
+                json.dump(pprofis, jsonFile)
+                jsonFile.close()
+
+            return jsonify(consultas)
+
+
+app.run(port=5000, host='localhost', debug=True)
